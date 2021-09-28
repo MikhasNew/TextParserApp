5eafb2b60073e76f8919875909df35c0af81c502
MikhasNew <87332000+MikhasNew@users.noreply.github.com>
2021-09-28 19:10:55 +0300

    Add worcing detector adn other.

---
+++ ConsoleWorcingDetector.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TextParserApp
+{
+    class ConsoleWorcingDetector
+    {
+        private int CurrentPosition { get; set; }
+        private bool IsShowed { get; set; }
+
+        public ConsoleWorcingDetector()
+        {
+            CurrentPosition = 0;
+            IsShowed = false;
+        }
+
+        public void ShowWorkingProcessDetecter()
+        {
+            Thread.Sleep(1000);
+            IsShowed = true;
+            while (IsShowed)
+            {
+
+                switch (CurrentPosition)
+                {
+                    case 0:
+                        Console.Write('\\');
+                        CurrentPosition = 1;
+                        break;
+                    case 1:
+                        Console.Write('|');
+                        CurrentPosition = 2;
+                        break;
+                    case 2:
+                        Console.Write('/');
+                        CurrentPosition = 3;
+                        break;
+                    default:
+                        Console.Write('-');
+                        CurrentPosition = 0;
+                        break;
+
+                }
+
+                var p = Console.GetCursorPosition();
+                if (p.Left != 0)
+                {
+                    Thread.Sleep(1000);
+                    Console.SetCursorPosition(p.Left - 1, p.Top);
+                }
+            }
+
+        }
+        public void ShowWorkingProcessDetecterAsync()
+        {
+            Thread t = new Thread(ShowWorkingProcessDetecter); //создает новый поток
+            t.Name = "2Thred";
+            t.Start(); //запускает новый поток
+
+        }
+        public void Stop()
+        {
+            IsShowed = false;
+            Console.WriteLine();
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,36 +13,42 @@ namespace TextParserApp
 {
     class Program
     {
+        private static string assemblyPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
 
-        static Dictionary<string, int> WordsCountDictyonary = new Dictionary<string, int>();
-        static Dictionary<char, int> LetterCountDyctionary = new Dictionary<char, int>();
-        static StringBuilder PunctList = new StringBuilder();
+        private static Dictionary<string, int> WordsCountDictyonary = new Dictionary<string, int>();
+        private static Dictionary<char, int> LetterCountDyctionary = new Dictionary<char, int>();
+        private static StringBuilder PunctList = new StringBuilder();
 
-        static int LongsteOffersOfSimbalsIndex;
-        static int LongsteOffersOfSimbalLength;
-        static int ShortestOffersOfWordsIndex;
-        static int ShortestOffersOfWordsCount;
+        private static int LongsteOffersOfSimbalsIndex;
+        private static int LongsteOffersOfSimbalLength;
+        private static int ShortestOffersOfWordsIndex;
+        private static int ShortestOffersOfWordsCount;
 
-        static int OffersNumber = 0;
+        private static int OffersNumber = 0;
 
-        static async Task Main( )
+        static async Task Main()
         {
+            ConsoleWorcingDetector myConsoleWorcingDetector = new ConsoleWorcingDetector();
+            myConsoleWorcingDetector.ShowWorkingProcessDetecterAsync();
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
             Console.WriteLine("Working...........");
 
-            string assemblyPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
             string resPath = assemblyPath + "\\Resource\\Sample.txt";
 
+            #region SreamConfig
             Stream streamOffers = new FileStream(assemblyPath + "\\Resource\\SampleOffers.txt", FileMode.Create);
             Stream streamWords = new FileStream(assemblyPath + "\\Resource\\SampleWords.txt", FileMode.Create);
             Stream streamWordsStatistic = new FileStream(assemblyPath + "\\Resource\\SampleWordsStatistic.txt", FileMode.Create);
             Stream streamPunctuationSymbols = new FileStream(assemblyPath + "\\Resource\\streamPunctuationSymbols.txt", FileMode.Create);
-            Stream streamOtherInformation = new FileStream(assemblyPath + "\\Resource\\streamOtherInformation.txt", FileMode.Create);
 
             StreamWriter streamWriterOffers = new StreamWriter(streamOffers, System.Text.Encoding.Unicode);
             StreamWriter streamWriterWords = new StreamWriter(streamWords, System.Text.Encoding.Unicode);
             StreamWriter streamWriterWordsStatistic = new StreamWriter(streamWordsStatistic, System.Text.Encoding.Unicode);
             StreamWriter streamWriterPunctuationSymbols = new StreamWriter(streamPunctuationSymbols, System.Text.Encoding.Unicode);
-            StreamWriter streamWriterOtherInformation = new StreamWriter(streamOtherInformation, System.Text.Encoding.Unicode);
+            #endregion
 
             string offersSplitPattern = @"(?<=(?<!Dr|Mr|Ms|St|aet|Fig|pp|(?:[A-Za-z]\.[A-Za-z])| [A-Z]|[A-Z]{2,})(?:[\.?!]{1,}[""*)}\]]{0,})(?=(?:[\s]{1,}[""]{0,1}[A-Z0-9*#_({[=])|$))";
             Regex offersSplitRegex = new Regex(offersSplitPattern);
@@ -54,7 +60,6 @@ namespace TextParserApp
             Regex punctRegex = new Regex(punctPattern);
 
             string tempString = null;
-
             try
             {
                 using (var sr = new StreamReader(resPath))
@@ -98,11 +103,8 @@ namespace TextParserApp
                         }
 
                         tempString = offersItems.Last();
-
                     }
-
-                    while (!sr.EndOfStream); 
-
+                    while (!sr.EndOfStream);
                 }
                 await streamWriterPunctuationSymbols.WriteLineAsync(PunctList);
 
@@ -114,43 +116,12 @@ namespace TextParserApp
                 await streamWriterOffers.FlushAsync();
                 await streamOffers.DisposeAsync();
 
-                using (StreamReader sr = new StreamReader(assemblyPath + "\\Resource\\SampleOffers.txt"))
-                {
-                    bool writteLong = false;
-                    bool writeCurz = false;
-
-                    int lineNumber = 0;
-
-                    var dict = LetterCountDyctionary.OrderBy(e => e.Value).Last();
-                    await streamWriterOtherInformation.WriteLineAsync($"The most common letter:\n {dict.Key} - {dict.Value}");
-
-                    while (sr != null)
-                    {
-                        var ci = await sr.ReadLineAsync();
-
-                        if (lineNumber == LongsteOffersOfSimbalsIndex)
-                        {
-                            //var longStr = await sr.ReadLineAsync();
-                            await streamWriterOtherInformation.WriteLineAsync($"This is a great offer for the number of characters:\n {ci}");
-                            writteLong = true;
-                        }
-                        if (lineNumber == ShortestOffersOfWordsIndex)
-                        {
-                            //var Str = await sr.ReadLineAsync();
-                            await streamWriterOtherInformation.WriteLineAsync($"This is the smallest sentence in terms of the number of words:\n {ci}");
-                            writeCurz = true;
-                        }
-                        if (writteLong && writeCurz)
-                            break;
-                        lineNumber++;
-
-                    }
-
-                }
+                await GenerateOtherInformation();
 
             }
             catch (Exception e)
             {
+                myConsoleWorcingDetector.Stop();
                 Console.WriteLine("\n -!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!");
                 Console.WriteLine($"\n {e}");
                 Console.WriteLine("\n -!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!");
@@ -159,22 +130,86 @@ namespace TextParserApp
             }
             finally
             {
+                myConsoleWorcingDetector.Stop();
                 await streamWriterPunctuationSymbols.FlushAsync();
                 await streamWriterWordsStatistic.FlushAsync();
                 await streamWriterWords.FlushAsync();
-                await streamWriterOtherInformation.FlushAsync();
 
                 await streamOffers.DisposeAsync();
                 await streamWords.DisposeAsync();
-                await streamOtherInformation.DisposeAsync();
                 await streamWordsStatistic.DisposeAsync();
                 await streamPunctuationSymbols.DisposeAsync();
+
+                timer.Stop();
+                var process = Process.GetCurrentProcess();
+                var maxMemoryUsed = process.PeakWorkingSet64;
+
                 Process.Start("explorer.exe", assemblyPath + "\\Resource\\");
+
+                Console.WriteLine($"This operation took {timer.ElapsedMilliseconds} ms.\nand used a max. {maxMemoryUsed} byte of memory.");
+                Console.ReadLine();
             }
-
-
         }
 
+        public static async Task GenerateOtherInformation()
+        {
+            Stream streamOtherInformation =
+                new FileStream(assemblyPath + "\\Resource\\streamOtherInformation.txt", FileMode.Create);
+            StreamWriter streamWriterOtherInformation =
+                new StreamWriter(streamOtherInformation, System.Text.Encoding.Unicode);
+            try
+            {
+                using (StreamReader sr = new StreamReader(assemblyPath + "\\Resource\\SampleOffers.txt"))
+                {
+                    bool writteLong = false;
+                    bool writeCurz = false;
+
+                    int lineNumber = 0;
+
+                    var dict = LetterCountDyctionary.OrderBy(e => e.Value).Last();
+                    await streamWriterOtherInformation.WriteLineAsync(
+                        $"The most common letter:\n {dict.Key} - {dict.Value + 1}");
+
+                    while (sr != null)
+                    {
+                        var ci = await sr.ReadLineAsync();
+
+                        if (lineNumber == LongsteOffersOfSimbalsIndex)
+                        {
+                            //var longStr = await sr.ReadLineAsync();
+                            await streamWriterOtherInformation.WriteLineAsync(
+                                $"This is a great offer for the number of characters:\n {ci}");
+                            writteLong = true;
+                        }
+
+                        if (lineNumber == ShortestOffersOfWordsIndex)
+                        {
+                            //var Str = await sr.ReadLineAsync();
+                            await streamWriterOtherInformation.WriteLineAsync(
+                                $"This is the smallest sentence in terms of the number of words:\n {ci}");
+                            writeCurz = true;
+                        }
+
+                        if (writteLong && writeCurz)
+                            break;
+                        lineNumber++;
+
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                await streamWriterOtherInformation.FlushAsync();
+                await streamWriterOtherInformation.DisposeAsync();
+            }
+
+        }
         public static void CheckLongOffersOfSimbals(int index, int length)
         {
             if (OffersNumber == 0)
@@ -203,7 +238,6 @@ namespace TextParserApp
             }
 
         }
-
         public static void AddToWordsDictionary(string word)
         {
             if (!int.TryParse(word, out _))
@@ -218,7 +252,6 @@ namespace TextParserApp
                 }
             }
         }
-
         public static void AddToletterCountDyctionary(char[] leters)
         {
             foreach (var leter in leters)
@@ -233,6 +266,9 @@ namespace TextParserApp
                 }
             }
         }
+
+
+
     }
 }
 
